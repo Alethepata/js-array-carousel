5d490077660bd39189d4a442063ae24f61585e9d
Alethepata <alessia.patane@gmail.com>
2023-09-13 16:38:40 +0200

    [Add] const

---
+++ js/script.js
@@ -0,0 +1,13 @@
+console.log('Ciao');
+
+const itemsWrapper = document.querySelector('.items-wrapper');
+const up = document.querySelector('.top');
+const down = document.querySelector('.bottom');
+
+const images = [
+    'img/01.webp',
+    'img/02.webp',
+    'img/03.webp',
+    'img/04.webp',
+    'img/05.webp'
+]
